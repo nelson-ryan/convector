b17132ae45a6af264d7f3e948281c32f8a0a71ef
Ryan Nelson <ryan@nelsonr.dev>
2025-12-02 19:20:58 -0700

    Boy howdy, gotta separate those

--- convector.py
+++ convector.py
@@ -1,5 +1,5 @@
 # convector
-from config import wikipath
+from config import wikipath, tokenized_output
 
 # py
 import os
@@ -16,21 +16,30 @@ import multiprocessing
 # immediately follows <doc opening,
 
 class Gobbler:
-    """ Wikipedia-dump-Gobbler
-        Serves as a preprocessor and iterator to feed into Word2Vec training
+    """ Preprocessor to tokenize and lemmatize Wikipedia dump files
     """
     NLP = None # lazy loading
-    def __init__(self, path: Path):
-        """ 
-            path (Path): parent path to extracted wikipedia dump
-            OR path to single wikipedia file
+    def __init__(self, wikipath: Path, outfile: Path = tokenized_output):
+        """ wikipath (Path): parent path to extracted wikipedia dump
+            OR path to single extracted wikipedia file (for testing)
+
+            out_file (Path): path for singular output, lemmatized text file
         """
-        self.wikipath = path
+        self.wikipath = wikipath
+        self.outfile = outfile
         if not Gobbler.NLP: # lazy loading of spacy pipeline
+            print("Loading SpaCy")
             Gobbler.NLP = spacy.load("en_core_web_md")
 
+    def gobble(self):
+        with open(self.outfile, mode = "w", encoding = "utf-8") as file:
+            for line in self:
+                file.write(" ".join(line) + "\n")
+
     def __iter__(self):
-        if self.wikipath.is_file():
+        if not isinstance(self.wikipath, Path):
+            raise TypeError
+        elif self.wikipath.is_file():
             yield from self._gobble_file(self.wikipath)
         elif self.wikipath.is_dir():
             yield from self._gobble_dir(self.wikipath)
@@ -41,17 +50,21 @@ class Gobbler:
         """ Reads all Wikipedia lines and produces lemmatized tokens.
             path (Path): individual wikipedia dump file
         """
+        print(f"Reading {path}")
         with open(path, mode = "r", encoding = "utf-8") as file:
-            for doc in Gobbler.NLP.pipe(file, batch_size = 50):
-                if len(doc) < 2: # should exclude titles alone on a line
+            for doc in Gobbler.NLP.pipe(file, batch_size = 100):
+                if re.match(pattern = r'^\w*$', string = doc.text):
+                    # should exclude titles alone on a line, and empty lines
                     continue
                 if re.match(pattern = r'^<\\?.*>$', string = doc.text):
-                    # exlude doc tags
+                    # exclude doc tags
                     continue
                 for sentence in doc.sents:
                     yield [
                         token.lemma_.lower() for token in sentence
-                        if not token.is_space and not token.is_punct
+                        if not token.is_space
+                        and not token.is_punct
+                        and not  token.is_stop
                     ]
 
     def _gobble_dir(self, path):
@@ -63,15 +76,31 @@ class Gobbler:
                 filepath = Path(root) / file
                 yield from self._gobble_file(filepath)
 
-if __name__ == '__main__':
-    wikidump = Gobbler(wikipath)
+class TrainingIterator:
+    def __init__(self, path: Path):
+        self.path = path
+    def __iter__(self):
+        with open(self.path) as file:
+            while line := file.readline():
+                yield line.strip().split(" ")
 
+if __name__ == '__main__':
+
+    print(wikipath)
+
+    # Preprocessing (tokenization/lemmatization) of Wikipedia extracted files
+    wikidump = Gobbler(wikipath)
+    wikidump.gobble()
+    wiki_processed = wikidump.outfile
+
+    # Train Word2Vec model
+    sentences = TrainingIterator(wiki_processed)
     model = Word2Vec(
-        sentences = wikidump,
+        sentences = sentences,
         sg = 0, # CBOW
         window = 5, # following Thurnbauer et al
         epochs = 5, # following Thurnbauer et al
         min_count = 10, # following Thurnbauer et al
-        workers = multiprocessing.cpu_count(), # all of dems!
+        workers = multiprocessing.cpu_count() - 1, # (nearly) all of dems!
     )
     model.save("convector.model")
